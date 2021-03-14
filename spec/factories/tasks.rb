18cea5cf5ecea59a4a3efc214a211d01d3423697
yorutaru <ryutafolder@horiuchiryuutaakiranoMacBook-Pro.local>
2021-03-14 18:51:43 +0900

    finish_cp_02

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :task do
-    
+    title { "test_title" }
+    status { "todo" }
+    association :user
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do 
+
+    before do
+      user = FactoryBot.build(:user)
+    end
+    
+    it 'is valid with all attributes' do 
+      task = FactoryBot.build(:task)
+      expect(task).to be_valid
+    end
+
+    it 'is invalid without title' do 
+      task = FactoryBot.build(:task, title: nil)
+      task.valid?
+      expect(task.errors[:title]).to include("can't be blank")
+    end
+
+    it 'is invalid without status' do
+      task = FactoryBot.build(:task, status: nil)
+      task.valid?
+      expect(task.errors[:status]).to include("can't be blank")
+    end
+
+    it 'is valid with another title' do 
+      FactoryBot.create(:task)
+      task = FactoryBot.build(:task, title: "test_title_02")
+      task.valid?
+      expect(task).to be_valid
+    end
+  end
 end
